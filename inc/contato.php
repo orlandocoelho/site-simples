c919da5171e9b0709144946e6e48f0608953107a
Orlando Coelho <email@orlandocoelho.com>
2014-08-20 21:29:22 -0300

    Adicionando formulario de contato e iniciando o php

--- inc/contato.php
+++ inc/contato.php
@@ -1,7 +1,52 @@
 <div class="jumbotron">
     <h1>CONTATO</h1>
     <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consectetur consequuntur dicta dolorem enim error hic in ipsa maiores minus, modi molestiae, mollitia omnis praesentium quasi repudiandae ullam velit vero. </p>
-    <p>
-        <a role="button" href="#" class="btn btn-lg btn-primary">BOTÃO PÁGINA CONTATO »</a>
-    </p>
 </div><!--./jumboton-->
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-offset-3 col-lg-6">
+
+            <?php
+
+
+                if(isset($envia) && $envia == "enviar"){
+
+                }
+            ?>
+
+            <form class="form-horizontal" role="form" action="" method="post">
+                <div class="form-group">
+                    <label for="nome" class="col-sm-2 control-label">Nome</label>
+                    <div class="col-sm-10">
+                        <input name="nome" type="text" class="form-control" placeholder="Seu nome..">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email" class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-10">
+                        <input name="email" type="email" class="form-control" placeholder="Seu email..">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="assunto" class="col-sm-2 control-label">Assunto</label>
+                    <div class="col-sm-10">
+                        <input name="assunto" type="assunto" class="form-control" placeholder="Assunto a tratar..">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="mensagem" class="col-sm-2 control-label">Mensagem</label>
+                    <div class="col-sm-10">
+                        <textarea name="mensagem" class="form-control" rows="3"></textarea>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" name="enviar" class="btn btn-default">Enviar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
