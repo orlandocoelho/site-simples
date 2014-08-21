9afefc8a538dc2bfc46747149dd9adfed8bc0e35
Orlando Coelho <email@orlandocoelho.com>
2014-08-20 21:28:42 -0300

    Correção de erros

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">SITE SIMPLEX</a>
+                    <a class="navbar-brand" href="#">SITE SIMPLES</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
