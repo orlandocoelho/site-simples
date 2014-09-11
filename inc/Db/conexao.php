915ffe32a43fedeef4032cf4c41fae1bf9a7479d
Orlando Coelho <email@orlandocoelho.com>
2014-09-11 19:00:00 -0300

    Arquivo de conexao com o banco de dados

---
+++ inc/Db/conexao.php
@@ -0,0 +1,25 @@
+<?php
+
+function conexao()
+{
+
+    try{
+        $config = require_once 'config.php';
+        if(!isset($config['db'])){
+            throw new \InvalidArgumentException("Configuração do bando de dados não existe!");
+        }
+
+        $host = (isset($config['db']['host'])) ? $config['db']['host'] : null;
+        $dbname = (isset($config['db']['dbname'])) ? $config['db']['dbname'] : null;
+        $user = (isset($config['db']['user'])) ? $config['db']['user'] : null;
+        $pass = (isset($config['db']['pass'])) ? $config['db']['pass'] : null;
+
+        return new \PDO("mysql:host={$host};dbname={$dbname}",$user,$pass);
+
+
+    }catch (\PDOException $e){
+        echo $e->getMessage()."\n";
+        echo $e->getTranceAsString()."\n";
+    }
+
+}
