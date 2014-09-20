16c674f7fd3db8c0d776c685a68e9eb0a678ffe4
Orlando Coelho <email@orlandocoelho.com>
2014-09-19 21:17:54 -0300

    Fixture add user

---
+++ admin/inc/fixture.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: orlando
+ * Date: 11/09/14
+ * Time: 18:55
+ */
+
+require_once "Db/conexao.php";
+
+$conn = conexao();
+
+
+$query = "CREATE DATABASE IF NOT EXISTS `sitesimple` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
+$stmp = $conn->prepare($query);
+if($stmp->execute()){
+    echo "ok db\n";
+}else{
+    echo "DB ja existe\n";
+}
+
+$create = $conn->query("CREATE TABLE IF NOT EXISTS `sitesimple`.`users` (
+                      `id` INT NOT NULL AUTO_INCREMENT,
+                      `login` VARCHAR(20) NULL,
+                      `senha` VARCHAR(255) NULL,
+                      PRIMARY KEY (`id`));");
+if($create){
+    echo "ok conteudo\n";
+}else{
+    echo "conteudo ja existe\n";
+}
+
+$senha = password_hash('123456', PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO `sitesimple`.`users` (`login`, `senha`) VALUES ('orlando', '$senha')";
+$stmt = $conn->prepare($sql);
+
+if(!$stmt->execute()):
+    die(var_dump($stmt->errorInfo()));
+else:
+    echo "Conteudo contato criado com sucesso ! \n";
+endif;
